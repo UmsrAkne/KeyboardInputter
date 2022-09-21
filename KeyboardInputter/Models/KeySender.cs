439e5bd25c29df1efe222f6ac7e71c56a5163cf3
Akn <sibata486@gmail.com>
2022-09-22 01:04:15 +0900

    init

---
+++ KeyboardInputter/Models/KeySender.cs
@@ -0,0 +1,41 @@
+namespace KeyboardInputter.Models;
+
+using System;
+using System.Runtime.InteropServices;
+
+public class KeySender
+{
+    public void SendKey(short keyCode)
+    {
+        var inputs = new[] {
+            new INPUT {
+            type = NativeMethods.INPUT_KEYBOARD,
+                ui = new INPUT_UNION {
+                    keyboard = new KEYBDINPUT {
+                        wVk = keyCode,
+                        wScan = (short) NativeMethods.MapVirtualKey (keyCode, 0),
+                        dwFlags = NativeMethods.KEYEVENTF_EXTENDEDKEY | NativeMethods.KEYEVENTF_KEYDOWN,    // キーの押し下げ
+                        dwExtraInfo = IntPtr.Zero,
+                        time = 0
+                    }
+                }
+            },
+            new INPUT {
+                type = NativeMethods.INPUT_KEYBOARD,
+                ui = new INPUT_UNION {
+                    keyboard = new KEYBDINPUT {
+                        wVk = keyCode,
+                        wScan = (short) NativeMethods.MapVirtualKey (keyCode, 0),
+                        dwFlags = NativeMethods.KEYEVENTF_EXTENDEDKEY | NativeMethods.KEYEVENTF_KEYUP,      // キーの押し上げ
+                        dwExtraInfo = IntPtr.Zero,
+                        time = 0
+                    }
+                }
+            }
+        };
+
+        NativeMethods.SendInput(2, ref inputs[0], Marshal.SizeOf(inputs[0]));
+    }
+
+
+}
---
+++ KeyboardInputter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using KeyboardInputter.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Timers;
+
+namespace KeyboardInputter.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private KeySender keySender = new KeySender();
+        private string title = "Prism Application";
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            var timer = new Timer();
+            timer.Interval = TimeSpan.FromMilliseconds(4500).TotalMilliseconds;
+            timer.Start();
+
+            timer.Elapsed += (_, _) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"MainWindowViewModel : inputKey");
+            };
+        }
+
+        public DelegateCommand KeyCommand => new DelegateCommand(() =>
+        {
+        });
+    }
+}
